6cdfb1a643aca4b297fd6ff7944e77e1bfc85ffb
Paulo Renato <paulorsouzaf@gmail.com>
2021-01-10 22:24:51 -0300

    :sparkles: Create basic cli arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::{App, AppSettings};
+
+mod constants;
+
 fn main() {
-    println!("Hello, world!");
+    let app_args = App::new("4Devs CLI")
+        .bin_name("4devs")
+        .version("0.0.1")
+        .author("PauloRSF <paulorsouzaf@gmail.com>")
+        .about("A CLI for 4Devs data generation services.")
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .get_matches();
+
+    let (sub, sub_args) = app_args.subcommand().unwrap();
+
+    match sub {
+        _ => (),
+    }
 }
